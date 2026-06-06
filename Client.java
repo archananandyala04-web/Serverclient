47bd06d1f2624281fdfc632710459bd95acfa772
archananandyala04-web <archananandyala04@gmail.com>
2026-06-06 09:53:04 +0530

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.net.*;
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("localhost", 1234);
+            DataOutputStream dos =
+                    new DataOutputStream(s.getOutputStream());
+            dos.writeUTF("Hello Server");
+            dos.flush();
+            dos.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket ss = new ServerSocket(1234);
+            System.out.println("Server Started");
+            Socket s = ss.accept();
+            System.out.println("Client Connected");
+            DataInputStream dis =
+                    new DataInputStream(s.getInputStream());
+            String msg = dis.readUTF();
+            System.out.println("Client Message: " + msg);
+            ss.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
